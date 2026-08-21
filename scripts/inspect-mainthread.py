98d5ad86e13da61b60c59c32a804f7f7e267947d
Lenin Meza <merolhack@gmail.com>
2026-08-21 11:47:54 -0600

    perf(css): load local stylesheets synchronously to eliminate LCP render delay

---
+++ scripts/inspect-mainthread.py
@@ -0,0 +1,15 @@
+import json
+
+with open('/mnt/c/Users/lenin/.gemini/antigravity-ide/brain/d2f7dd85-7a98-4472-91dc-8f7f0c4f276c/.system_generated/tasks/task-1245.log', 'r') as f:
+    text = f.read()
+
+start = text.find('{')
+end = text.rfind('}')
+if start != -1 and end != -1:
+    data = json.loads(text[start:end+1])
+    work = data['audits']['mainthread-work-breakdown']
+    print("Mainthread work breakdown:")
+    print(json.dumps(work.get('details', {}), indent=2))
+    print("\nBootup time:")
+    bootup = data['audits']['bootup-time']
+    print(json.dumps(bootup.get('details', {}), indent=2))
